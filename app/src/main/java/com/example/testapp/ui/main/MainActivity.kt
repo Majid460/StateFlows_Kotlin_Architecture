14f7a06fe56364d1506c874d5289c27096498d55
majid.shahbaz <majid.shahbaz@nxb.com.pk>
2023-10-12 19:01:30 +0500

    Added room with flows

--- app/src/main/java/com/example/testapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/testapp/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testapp.ui.main
 
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -9,29 +10,39 @@ import com.example.testapp.R
 import com.example.testapp.base.BaseActivity
 import com.example.testapp.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
     private lateinit var  mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainViewModel= ViewModelProvider(this)[MainViewModel::class.java]
-        mainViewModel.getBooks()
-        mainViewModel.getAllAuthors()
 
-        lifecycleScope.launch {
+        mainViewModel.getAllAuthors()
+        val btnFetch=findViewById<AppCompatButton>(R.id.btn)
+        val btnDelete = findViewById<AppCompatButton>(R.id.btn1)
+        btnDelete.setOnClickListener {
+            mainViewModel.deleteBooks()
+        }
+        btnFetch.setOnClickListener {
+            mainViewModel.getBooks()
+        }
+
+        lifecycleScope.launch(Dispatchers.Default) {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 mainViewModel.booksStateFlow.collect {
                     when (it.status) {
-                         Status.SUCCESS -> {println("Shared Flow Books:: "+it.data)}
-                         Status.ERROR -> {println("Shared Flow Error:: "+it.message)}
+                        Status.SUCCESS -> {println("Shared Flow Books:: "+it.data)}
+                        Status.ERROR -> {println("Shared Flow Error:: "+it.message)}
                         else -> {}
                     }
                 }
             }
         }
-        lifecycleScope.launch {
+        lifecycleScope.launch(Dispatchers.Default) {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 mainViewModel.authorsStateFlow.collect {
                     when (it.status) {
@@ -42,9 +53,7 @@ class MainActivity : BaseActivity() {
                 }
             }
         }
-
     }
-
     override fun getLayoutResourceId(): Int {
         return R.layout.activity_main
     }
--- app/src/main/java/com/example/testapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/testapp/ui/main/MainViewModel.kt
@@ -3,10 +3,11 @@ package com.example.testapp.ui.main
 import androidx.lifecycle.viewModelScope
 import com.example.testapp.base.BaseViewModel
 import com.example.testapp.data.Repository
+import com.example.testapp.data.localData.Database
 import com.example.testapp.data.models.authors.AuthorsResponseModel
-import com.example.testapp.data.models.books.BooksResponse
 import com.example.testapp.utils.NetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
@@ -14,41 +15,34 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val repo:Repository): BaseViewModel() {
- private val mutableStateData= MutableStateFlow<NetworkResult<MutableList<BooksResponse>?>>(NetworkResult.loading())
+class MainViewModel @Inject constructor(private val repo:Repository,private val localData: Database): BaseViewModel() {
  private val authorsData= MutableStateFlow<NetworkResult<MutableList<AuthorsResponseModel>?>>(NetworkResult.loading())
-   val booksStateFlow:StateFlow<NetworkResult<MutableList<BooksResponse>?>> = mutableStateData
+   val booksStateFlow = repo.getBooksLocally()
    val authorsStateFlow:StateFlow<NetworkResult<MutableList<AuthorsResponseModel>?>> = authorsData
-
-    fun getBooks(){
-        viewModelScope.launch {
-            repo.getBooksData().catch {
-                mutableStateData.value= it.message?.let { it1 -> NetworkResult.error(it1) }!!
-            }.collect{
-                if(it.data != null){
-
-                    mutableStateData.value=NetworkResult.success(it.data)
-                }else{
-                    mutableStateData.value= it.message?.let { it1 -> NetworkResult.error(it1) }!!
-
+    fun deleteBooks() = viewModelScope.launch(Dispatchers.IO) {
+        repo.deleteData()
+    }
+    fun getBooks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.getBooksData().collect {
+                if (it.data != null) {
+                    localData.getDao().insertAll(it.data)
                 }
             }
         }
     }
-
-    fun getAllAuthors(){
-        viewModelScope.launch {
-            repo.getAuthorsData().catch {
-             authorsData.value= it.message?.let { it1 -> NetworkResult.error(it1) }!!
-            }.collect{
-             if(it.data!=null){
-                 authorsData.value= NetworkResult.success(it.data)
-             }
-             else{
-                 authorsData.value= it.message?.let { it1 -> NetworkResult.error(it1) }!!
-             }
+        fun getAllAuthors() {
+            viewModelScope.launch {
+                repo.getAuthorsData().catch {
+                    authorsData.value = it.message?.let { it1 -> NetworkResult.error(it1) }!!
+                }.collect {
+                    if (it.data != null) {
+                        authorsData.value = NetworkResult.success(it.data)
+                    } else {
+                        authorsData.value = it.message?.let { it1 -> NetworkResult.error(it1) }!!
+                    }
+                }
             }
         }
-    }
 
 }
